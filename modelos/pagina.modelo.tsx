d521252731b79c62f0385209080a438b389ccd89
Edson Santos <edson.santos@gmail.com>
2021-04-26 09:59:25 -0300

    Criando Página Modelo

---
+++ modelos/pagina.modelo.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import {
+    SafeAreaView,
+    StyleSheet,
+    View,
+    Text
+} from 'react-native';
+
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+export function NomedaFuncao(){
+
+    return(){
+
+        <SafeAreaView style={styles.container}>
+            <View style={styles.content}>
+                <Text style={styles.text}>
+                    O texto
+                </Text>
+            </View>
+        </SafeAreaView>
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    content:{
+        flex: 1
+    }
+    text:{
+
+    }
+})
